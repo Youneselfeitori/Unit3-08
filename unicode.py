1f64ad3f5760dec3769c16f1e14cdc69b76f1d5e
Younes Elfeitori <youneselfeitori@gmail.com>
2017-10-29 19:47:09 -0400

    Init

---
+++ unicode.py
@@ -0,0 +1,13 @@
+#Created by: Younes Elfeitori
+#Created on: OCT 2017
+#Created for:ICS3U
+#This program runs a unicode system 
+
+for counter in range(65,91):
+   #output
+    print(str(counter)+ " => " + unichr(counter))
+
+
+for counter in range(97,123):
+   #output
+    print(str(counter)+ " => " + unichr(counter))
